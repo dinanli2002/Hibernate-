3a9622857f4b10ffbd522acbb663e3f1bcf42f40
dinanli2002 <dinan.li2003@gmail.com>
2024-03-05 18:44:23 +0100

    1#

--- DAM2T-M06-UF2-P2-Viticulture/src/model/Entrada.java
+++ DAM2T-M06-UF2-P2-Viticulture/src/model/Entrada.java
@@ -2,20 +2,34 @@ package model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
 @Table(name= "Entrada")
 public class Entrada {
 	@Id
-	@Column(name = "id")
-	private String valor;
-
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name = "id", unique = true, nullable = true)
+	private int id;
 	@Column(name = "instruccion")
 	private String instruccion;
-	
+	@OneToMany //This especifies that for every instance os this class we hav emany of the other
+    @JoinColumn(name = "Entrada_id")
+	public int getId() {
+		return id;
+	}
 	public String getInstruccion() {
-		return this.instruccion;
+		return instruccion;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public void setInstruccion(String instruccion) {
+		this.instruccion = instruccion;
 	}
 }
